0a7888c0b584a179bafa89204d363e616f5bb285
KoToZ <mohamedtareck95@gmail.com>
2017-07-26 15:15:58 +0200

    Update qsort.c

--- data structure/qsort/qsort.c
+++ data structure/qsort/qsort.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "test.h"
+#include <time.h>
+
 void main(int argc, char const *argv[])
 {
+	clock_t begin = clock();
 	int Array[]={3,2,4,5,1,9};
 	int size = sizeof(Array)/sizeof(int);
 	bubble_sort(Array,size,compare);
@@ -9,4 +12,7 @@ void main(int argc, char const *argv[])
 	{
 		printf("%d\n",Array[i] );
 	}
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("code time excution %f\n",time_spent );
 }
