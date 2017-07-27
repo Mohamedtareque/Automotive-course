e5664ff0044cf1c9f5c140852d731f4139844dd8
KoToZ <mohamedtareck95@gmail.com>
2017-07-27 04:49:00 +0200

    Create io.h

---
+++ AVR Drivers Code/AVR AT32L/io.h
@@ -0,0 +1,112 @@
+#ifndef IO_H
+#define IO_H 
+
+#define 	PORTA0 		 40
+#define 	PORTA1 		 39
+#define 	PORTA2		 38
+#define 	PORTA3		 37
+#define 	PORTA4		 36
+#define 	PORTA5		 35
+#define 	PORTA6		 34
+#define 	PORTA7 		 33
+
+
+#define 	PORTB0 		 1
+#define 	PORTB1 		 2
+#define 	PORTB2		 3
+#define 	PORTB3		 4
+#define 	PORTB4		 5
+#define 	PORTB5		 6
+#define 	PORTB6		 7
+#define 	PORTB7 		 8 
+
+#define 	PORTC0 		 22
+#define 	PORTC1 		 23
+#define 	PORTC2		 24
+#define 	PORTC3		 25
+#define 	PORTC4		 26
+#define 	PORTC5		 27
+#define 	PORTC6		 28
+#define 	PORTC7 		 29
+
+
+#define 	PORTD0 		 14
+#define 	PORTD1 		 15
+#define 	PORTD2		 16
+#define 	PORTD3		 17
+#define 	PORTD4		 18
+#define 	PORTD5		 19
+#define 	PORTD6		 20
+#define 	PORTD7 		 21 
+
+
+#ifndef PA0 
+#define PA0 PORTA0
+#ifndef PA1 
+#define PA1 PORTA1
+#ifndef PA2 
+#define PA2 PORTA2
+#ifndef PA3 
+#define PA3 PORTA3
+#ifndef PA4 
+#define PA4 PORTA4
+#ifndef PA5 
+#define PA5 PORTA5
+#ifndef PA6 
+#define PA6 PORTA6
+#ifndef PA7 
+#define PA7 PORTA7
+
+#ifndef PB0 
+#define PB0 PORTB0
+#ifndef PB1 
+#define PB1 PORTB1
+#ifndef PB2 
+#define PB2 PORTB2
+#ifndef PB3 
+#define PB3 PORTB3
+#ifndef PB4 
+#define PB4 PORTB4
+#ifndef PB5 
+#define PB5 PORTB5
+#ifndef PB6 
+#define PB6 PORTB6
+#ifndef PB7 
+#define PB7 PORTB7
+
+#ifndef PC0 
+#define PC0 PORTC0
+#ifndef PC1 
+#define PC1 PORTC1
+#ifndef PC2 
+#define PC2 PORTC2
+#ifndef PC3 
+#define PC3 PORTC3
+#ifndef PC4 
+#define PC4 PORTC4
+#ifndef PC5 
+#define PC5 PORTC5
+#ifndef PC6 
+#define PC6 PORTC6
+#ifndef PC7 
+#define PC7 PORTC7
+
+#ifndef PD0 
+#define PD0 PORTD0
+#ifndef PD1 
+#define PD1 PORTD1
+#ifndef PD2 
+#define PD2 PORTD2
+#ifndef PD3 
+#define PD3 PORTD3
+#ifndef PD4 
+#define PD4 PORTD4
+#ifndef PD5 
+#define PD5 PORTD5
+#ifndef PD6 
+#define PD6 PORTD6
+#ifndef PD7 
+#define PD7 PORTD7
+
+void pinMode(int port,obj* state)
+#endif
