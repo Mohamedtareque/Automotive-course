01eac10f696af10b57e0d31d76f8013526df0c18
KoToZ <mohamedtareck95@gmail.com>
2017-07-27 04:49:21 +0200

    Create sfr.h

---
+++ AVR Drivers Code/AVR AT32L/sfr.h
@@ -0,0 +1,15 @@
+#ifndef SFR_H
+#define SFR_H 
+
+#define		 BASE_A 	0x39 
+#define 	 BASE_B 	0x36
+#define 	 BASE_C 	0x33
+#define  	 BASE_D 	0x30
+
+#define 	OFFSET_INPUT 	0x00
+#define 	OFFSET_DIR   	0x01
+#define 	OFFSET_PORT  	0x02
+
+
+
+#endif
