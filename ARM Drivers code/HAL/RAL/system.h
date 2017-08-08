8caff552fc1bac0dcac4cc6b1ad757e1ee808bc2
KoToZ <mohamedtareck95@gmail.com>
2017-08-08 20:07:37 +0200

    Create system.h

---
+++ ARM Drivers code/HAL/RAL/system.h
@@ -0,0 +1,15 @@
+/*
+ * system.h
+ *
+ *  Created on: Aug 7, 2017
+ *      Author: Mohamed
+ */
+
+#ifndef HAL_RAL_SYSTEM_H_
+#define HAL_RAL_SYSTEM_H_
+#include"types.h"
+#define 		RCGCGPIO 			(*((volatile uint32_t *)0x400FE608))
+
+
+
+#endif /* HAL_RAL_SYSTEM_H_ */
