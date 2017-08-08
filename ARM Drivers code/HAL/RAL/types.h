122c241544d4ff989f55635d544a1142f7a15296
KoToZ <mohamedtareck95@gmail.com>
2017-08-08 20:07:55 +0200

    Create types.h

---
+++ ARM Drivers code/HAL/RAL/types.h
@@ -0,0 +1,18 @@
+/*
+ * types.h
+ *
+ *  Created on: Aug 7, 2017
+ *      Author: Mohamed
+ */
+
+#ifndef HAL_RAL_TYPES_H_
+#define HAL_RAL_TYPES_H_
+
+typedef unsigned char uint8_t;
+typedef unsigned long uint32_t;
+typedef unsigned short int uint16_t;
+
+
+
+
+#endif /* HAL_RAL_TYPES_H_ */
